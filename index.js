54cfa9351ed385f3b6a2e3b9fa89a7b9f78e6aa5
nayanendu naskar <nayan19981@gmail.com>
2022-08-23 16:20:35 +0530

    final done

---
+++ index.js
@@ -0,0 +1,43 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value > billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+
+        } else {
+            showMessage("The cash provided should atleast be equal to the bill amount");
+        }
+
+    } else {
+        showMessage("Invalid bill amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(
+        amountToBeReturned / availableNotes[i]
+    );
+    amountToBeReturned %= availableNotes[i];
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText =numberOfNotes;
+}
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
