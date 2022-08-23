b9e75560dc1459da74b191f1d8e691ba3be85c8b
nayanendu naskar <nayan19981@gmail.com>
2022-08-23 17:29:56 +0530

    changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
 hideMessage();
     if (billAmount.value > 0) {
-        if (cashGiven.value > billAmount.value) {
+        if (cashGiven.value >= billAmount.value) {
 
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
@@ -28,7 +28,7 @@ for (let i = 0; i < availableNotes.length; i++) {
     const numberOfNotes = Math.trunc(
         amountToBeReturned / availableNotes[i]
     );
-    amountToBeReturned %= availableNotes[i];
+
     amountToBeReturned = amountToBeReturned % availableNotes[i];
     noOfNotes[i].innerText =numberOfNotes;
 }
